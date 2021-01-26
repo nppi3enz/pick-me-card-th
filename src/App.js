b0e02d92a4e778ead2128584d7c60cb5170f5453
Nipitpon Chantada <nipitpon.cha@ascendcorp.com>
2021-01-27 02:46:39 +0700

    test css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,31 @@ import {
   useRouteMatch,
   useParams
 } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css'
+
+import { Card, Icon, Image } from 'semantic-ui-react'
+
+const CardExampleCard = () => (
+  <Card>
+    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+)
+
 
 export default function App() {
   return (
@@ -41,7 +66,12 @@ export default function App() {
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h2>Home</h2>
+      <CardExampleCard/>
+    </div>
+  )
 }
 
 function About() {
